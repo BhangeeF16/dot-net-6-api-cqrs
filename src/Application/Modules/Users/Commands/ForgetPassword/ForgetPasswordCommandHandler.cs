f8b86b7d8130c833345865683881821498dd3c01
Aftab Ali <aftab.ali@reporteq.com>
2023-08-29 18:42:35 +0500

    - removed errors in forget password

--- src/Application/Modules/Users/Commands/ForgetPassword/ForgetPasswordCommandHandler.cs
+++ src/Application/Modules/Users/Commands/ForgetPassword/ForgetPasswordCommandHandler.cs
@@ -2,6 +2,7 @@
 using Domain.Common.Constants;
 using Domain.Common.Exceptions;
 using Domain.Common.Extensions;
+using Domain.ConfigurationOptions;
 using MediatR;
 using Utilities.Abstractions;
 using Utilities.Models;
@@ -12,8 +13,9 @@ namespace Application.Modules.Users.Commands.ForgetPassword;
 public class ForgetPasswordCommandHandler : IRequestHandler<ForgetPasswordCommand, bool>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly ApplicationOptions _options;
     private readonly IEmailService _emailService;
-    public ForgetPasswordCommandHandler(IUnitOfWork unitOfWork, IEmailService emailService) => (_unitOfWork, _emailService) = (unitOfWork, emailService);
+    public ForgetPasswordCommandHandler(IUnitOfWork unitOfWork, IEmailService emailService, ApplicationOptions options) => (_unitOfWork, _emailService, _options) = (unitOfWork, emailService, options);
 
     public async Task<bool> Handle(ForgetPasswordCommand request, CancellationToken cancellationToken)
     {
@@ -35,12 +37,9 @@ public class ForgetPasswordCommandHandler : IRequestHandler<ForgetPasswordComman
 
             _emailService.SendEmailAsync(new EmailOptions
             {
-                Subject = "Farmers Pick - Forget Password Request",
+                Subject = $"{_options.Company} - Forget Password Request",
                 IsBodyHtml = true,
-                ToEmails = new List<string>()
-                {
-                    user.Email!
-                },
+                ToEmails = new List<string>() { user.Email! },
                 PlaceHolders = new List<KeyValuePair<string, string>>()
                 {
                     new KeyValuePair<string, string>("{{USERNAME}}", user.FirstName! + " " + user.LastName!),
