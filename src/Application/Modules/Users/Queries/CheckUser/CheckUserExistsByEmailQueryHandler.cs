e75b35b2c98222e29301c9e6f3cc5b9976bbff74
Aftab Ali <aftab.ali@reporteq.com>
2023-09-20 13:18:36 +0500

    - error fixes

--- src/Application/Modules/Users/Queries/CheckUser/CheckUserExistsByEmailQueryHandler.cs
+++ src/Application/Modules/Users/Queries/CheckUser/CheckUserExistsByEmailQueryHandler.cs
@@ -3,13 +3,16 @@ using MediatR;
 
 namespace Application.Modules.Users.Queries.CheckUser;
 
-public class CheckUserExistsByEmailQueryHandler : IRequestHandler<CheckUserExistsByEmailQuery, bool>
+public class CheckUserExistsByEmailQueryHandler : IRequestHandler<CheckUserExistsByEmailQuery, CheckUserExistsByEmailQueryResponse>
 {
     private readonly IUnitOfWork _unitOfWork;
     public CheckUserExistsByEmailQueryHandler(IUnitOfWork UnitOfWork) => _unitOfWork = UnitOfWork;
 
-    public async Task<bool> Handle(CheckUserExistsByEmailQuery request, CancellationToken cancellationToken)
+    public async Task<CheckUserExistsByEmailQueryResponse> Handle(CheckUserExistsByEmailQuery request, CancellationToken cancellationToken)
     {
-        return await _unitOfWork.Users.ExistsAsync(x => x.Email.ToLower().Equals(request.Email.ToLower()) && x.IsActive && !x.IsDeleted);
+        var user = await _unitOfWork.Users.GetFirstOrDefaultAsync(x => x.Email.ToLower().Equals(request.Email.ToLower()) && x.IsActive && !x.IsDeleted);
+        return user == null
+            ? new CheckUserExistsByEmailQueryResponse(false, null)
+            : new CheckUserExistsByEmailQueryResponse(!string.IsNullOrEmpty(user.Email), !user.IsOTPLogin);
     }
 }
